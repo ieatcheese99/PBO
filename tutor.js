b7640c570d6f2b85703e8917f302e41a77a787da
ieatcheese99 <harjatizabdan@gmail.com>
2025-08-04 15:07:27 +0700

    Add files via upload

--- tutor.js
+++ tutor.js
@@ -68,21 +68,119 @@
 // sepedaA.start();
 // sepedaB.brake();
 
-let sepeda = {
-  merk: "wimcycle",
-  jumlah_gigi: 7,
-  warna_sepeda: "hitam",  
+// let sepeda = {
+//   merk: "wimcycle",
+//   jumlah_gigi: 7,
+//   warna_sepeda: "hitam",  
 
 
-  start: function() {
-    console.log("sepeda di goes.");
-  },
-  brake: function() {
-    console.log("sepeda mengerem.");
-  },
-  stop: function() {
-    console.log("sepeda berhenti.");
+//   start: function() {
+//     console.log("sepeda di goes.");
+//   },
+//   brake: function() {
+//     console.log("sepeda mengerem.");
+//   },
+//   stop: function() {
+//     console.log("sepeda berhenti.");
+//   }
+// };
+// console.log("Nama mobil: " + sepeda.merk);
+// sepeda.start();
+
+// function Car(name, model, color, weight) {
+//   this.name = name;
+//   this.model = model;
+//   this.color = color;
+//   this.weight = weight;
+
+//   this.start = function() {
+//     console.log(`${this.name} mulai.`);
+//   };
+
+//   this.drive = function() {
+//     console.log(`${this.name} jalan.`);
+//   };
+// }
+
+// let mobil1 = new Car("Toyota", "Camry", "Merah", "2000kg");
+// let mobil2 = new Car("Mio", "Camry", "Merah", "2000kg");
+
+// mobil1.start();  // Output: Toyota dinyalakan.
+// mobil2.drive();  // Output: Toyota sedang berjalan
+
+
+// class car{
+//   constructor(name, model, color, weight) {
+//     this.name = name;
+//     this.model = model;
+//     this.color = color;
+//     this.weight = weight;
+//   }
+
+//   start() {
+//     console.log(`${this.name} mulai.`);
+//   }
+//   drive() {
+//     console.log(`${this.name} jalan.`);
+//   }
+// }
+
+// let mobil1 = new car("Toyota", "Camry", "Merah", "2000kg");
+// let mobil2 = new car("Mio", "Camry", "Merah", "2000kg");
+
+// mobil1.start();  // Output: Toyota dinyalakan.
+// mobil2.drive();  // Output: Toyota sedang berjalan
+
+// class Sepeda {
+//   constructor(merk, jumlah_gigi, warna_sepeda) {
+//     this.merk = merk;
+//     this.jumlah_gigi = jumlah_gigi;
+//     this.warna_sepeda = warna_sepeda;
+//   }
+
+//   start() {
+//     console.log(`Sepeda ${this.merk} digoes.`);
+//   }
+
+//   brake() {
+//    console.log(`Sepeda ${this.merk} mengrem.`);
+//   }
+
+//   stop() {
+//     console.log(`Sepeda ${this.merk} berjenti.`);
+//   }
+// }
+
+// // Membuat objek dari class
+// let sepeda = new Sepeda("Aguscycle", 7, "Hitam");
+// let sepeda2 = new Sepeda("bycicle", 10, "Hijau");
+
+// sepeda.start();   // Output: Sepeda Wimcycle digoes.
+// sepeda2.brake();  // Output: Sepeda mengerem.
+
+  class Mahasiswa {
+  constructor(nama, NIM, jurusan, tahun_angkatan) {
+    this.nama = nama;
+    this.NIM = NIM;
+    this.jurusan = jurusan;
+    this.tahun_angkatan = tahun_angkatan;
   }
-};
-console.log("Nama mobil: " + sepeda.merk);
-sepeda.start();
+
+  introduce() {
+    console.log(`Halo, nama saya ${this.nama}, mahasiswa jurusan ${this.jurusan}, angkatan ${this.tahun_angkatan}.`);
+  }
+
+  issenior() {
+    return this.tahun_angkatan < 2025;
+  }
+}
+
+
+let mahasiswa1 = new Mahasiswa("Agus", 201921013, "Sastra Jawa", 2024); 
+let mahasiswa2 = new Mahasiswa("Agussss", 202921013, "Teknik Mancing", 2026);
+
+
+mahasiswa1.introduce();
+mahasiswa2.introduce();    
+console.log(mahasiswa1.issenior()); 
+console.log(mahasiswa2.issenior()); 
